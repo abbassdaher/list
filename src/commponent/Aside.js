9f151cc902878b12d29a76ce08538c6b5f4525a2
abbassDaher <abbassDaher1@outlook.com>
2024-06-19 14:32:19 +0300

    use map function to rendering the posts and tag button

--- src/commponent/Aside.js
+++ src/commponent/Aside.js
@@ -2,12 +2,15 @@ import React from 'react'
 import TagButton from './TagButton'
 
 function Aside() {
+    let content = [
+        { title: "جديدة", child: "🍌🍌🍌" },
+        { title: "الأكثر قراءة", child: <div><img src="https://images.pexels.com/photos/56866/garden-rose-red-pink-56866.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="flowers" style={{ width: '100x', height: '50px' }} /></div> },
+        { title: "جديدة", child: <div><img src="https://images.pexels.com/photos/85773/pexels-photo-85773.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="yellow" style={{ width: '100x', height: '50px' }} /></div> },
+    ]
     return (
         <>
             <div className='borderStyle asideStyle'>
-                <TagButton content="جديدة">🍌🍌🍌</TagButton>
-                <TagButton content="الأكثر قراءة"> <div><img src="https://images.pexels.com/photos/56866/garden-rose-red-pink-56866.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="flowers" style={{ width: '100x', height: '50px' }} /></div></TagButton>
-                <TagButton content="مقالات الأسبوع"><div><img src="https://images.pexels.com/photos/85773/pexels-photo-85773.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="yellow" style={{ width: '100x', height: '50px' }}/></div></TagButton>
+                {content.map((el, i) => <TagButton key={i} content={el.title}>{el.child}</TagButton>)}
 
             </div>
         </>
--- src/commponent/Body.js
+++ src/commponent/Body.js
@@ -2,15 +2,22 @@ import React from 'react'
 import Post from './Post'
 
 function Body() {
-    const content = { title: "lebanon", paragraph: "lebanon nebanon lebaon" }
+    const content = [
+        { title: "lebanon", paragraph: "lebanon nebanon lebaon" },
+        { title: "بحار الأنوار - العلامة المجلسي الصفحة ١٧٠", paragraph: "كتاب دلائل الإمامة للطبري'", child: <h2>ج ٤٣ </h2> },
+        { title: "blossom", paragraph: "is the reproductive structure found in flowering plants" }
+    ]
     return (
         <div >
             <div className='postStyle'>
-                <Post title="lebanon" paragraph="lebanon nebanon lebaon" />
-                <Post title='بحار الأنوار - العلامة المجلسي الصفحة ١٧٠' paragraph=' كتاب دلائل الإمامة للطبري'>
-                    <h2>ج ٤٣ </h2>
-                </Post>
-
+                {content.map((item, index) => {
+                    return <Post key={index} title={item.title} paragraph={item.paragraph} >{item.child}</Post>
+                })}
+                {/*comment*/}
+                {/*<Post title="lebanon" paragraph="lebanon nebanon lebaon" />*/}
+                {/*<Post title='بحار الأنوار - العلامة المجلسي الصفحة ١٧٠' paragraph=' كتاب دلائل الإمامة للطبري'>*/}
+                {/*<h2>ج ٤٣ </h2>*/}
+                {/*</Post>*/}
             </div>
         </div>
     )
